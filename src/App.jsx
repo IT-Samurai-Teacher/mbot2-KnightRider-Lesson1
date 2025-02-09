1da1183f69e011fb8a40e5d5d0216cce7663985b
IT Samurai Teacher <127447902+IT-Samurai-Teacher@users.noreply.github.com>
2025-02-08 16:57:19 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MBot2Demo from './components/MBot2Demo'
+
+function App() {
+  return (
+    <div className="App">
+      <MBot2Demo />
+    </div>
+  )
+}
+
+export default App
